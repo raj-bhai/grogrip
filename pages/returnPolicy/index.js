e39e013f5f3e2b51a1bd044ce1058b6909dd0772
Rajkiran Kalowar <rajkirankalowar@Rajkirans-MacBook-Pro.local>
2023-07-21 12:47:36 +0530

    return-policy

--- pages/returnPolicy/index.js
+++ pages/returnPolicy/index.js
@@ -7,30 +7,91 @@ function ReturnPolicy() {
         <div className="flex flex-col items-center h-screen" style={{ height: "100vh" }} >
             <Head>
                 <title>Return Policy</title>
-                <meta name="description" content="Criativcity Return Policy" />
+                <meta name="description" content="Grogrip Return Policy" />
                 {/* Other meta tags */}
             </Head>
-            <div className="max-w-3xl w-full px-4 py-8">
+            <div className="max-w-full w-full flex flex-col items-center px-8 py-8 ">
                 <h1 className="text-3xl font-bold mb-4">Return Policy</h1>
-                <p className="mb-2">
-                    Thank you for shopping at www.criativcity.com
-                </p>
-                <p className="mb-2">
-                    Non-tangible irrevocable goods (&quot;Digital products&quot;)
-                    We do issue refunds for non-tangible irrevocable goods (&quot;digital products&quot;) once the order is confirmed and the
-                    product is sent.
-                    We recommend contacting us for assistance if you experience any issues receiving or downloading our products.
-                </p>
-                <p className="mb-2">
-                    Certainly, we understand that clarity is important when it comes to refunds. If you decide to cancel your course within
-                    the 7-day window and are eligible for a refund, we will process a full refund of the amount you paid for the course.
-                    For example, if the course costs 1000 Rs, you will receive a full refund of 1000 Rs. Our goal is to provide our
-                    customers with a worry-free purchasing experience, and we are committed to upholding this policy. Please don&apos;t hesitate
-                    to contact us if you have any further questions or concerns.
-                </p>
-                <p className="mb-2">
-                    If you have any questions about our Returns and Refunds Policy, please contact us at: support@criativcity.com
-                </p>
+
+                <div className=" w-full mb-[10px] " >
+                    <p className=" font-bold " >1. Eligibility</p>
+                    <p>
+                        1.1 GROGRIP MEDIA ("Company," "we," "our," or "us") offers a return policy for YouTube Automation Services provided to our clients ("Client," "you," or "your") under certain circumstances. This policy applies only to specific situations as outlined below.
+                    </p>
+                </div>
+
+                <div className=" w-full mb-[10px] " >
+                    <p className=" font-bold " >2. Eligible Return Situations</p>
+                    <p>
+                        2.1 Clients are eligible for returns in the following scenarios: a) Unsatisfactory Video Quality: If you find that the delivered video does not meet the agreed-upon quality standards and requirements, you have the option to request a return.
+                    </p>
+                </div>
+
+
+                <div className=" w-full mb-[10px] " >
+                    <p className=" font-bold " >3. Return Procedure</p>
+                    <p>
+                        3.1 To initiate a return, the Client must follow these steps:
+                    </p>
+                    <p>
+                        A. Contact GROGRIP MEDIA's customer support within 7 days from the delivery date of the video.
+                    </p>
+                    <p>
+                        B. Provide specific reasons for the return request and any relevant information or evidence to support your claim.
+                    </p>
+                </div>
+
+                <div className=" w-full mb-[10px] " >
+                    <p className=" font-bold " >4. Refund Options</p>
+                    <p>
+                        4.1 In eligible return situations, the Client has the following refund options:
+                    </p>
+                    <p>
+                        A. Full Refund: GROGRIP MEDIA will provide a 100% refund of the paid amount for the Services.
+                    </p>
+                    <p>
+                        B. Revisions: If the Client prefers, they can opt for unlimited revisions to the video until their requirements are met, instead of a refund.
+                    </p>
+                </div>
+
+                <div className=" w-full mb-[10px] " >
+                    <p className=" font-bold " >5. Non-Returnable Scenarios</p>
+                    <p>
+                        5.1 The return policy is not applicable in the following situations:
+                    </p>
+                    <p>
+                        A. Content Uploaded on YouTube: If the Client uploads the video on YouTube and then requests a return, no refund will be provided due to the registration of content ID on YouTube.
+                    </p>
+                </div>
+
+                <div className=" w-full mb-[10px] " >
+                    <p className=" font-bold " >6. Processing Time</p>
+                    <p>
+                        6.1 GROGRIP MEDIA will process eligible return requests within [number of days] days from the date the request is received and approved.
+                    </p>
+                </div>
+
+                <div className=" w-full mb-[10px] " >
+                    <p className=" font-bold " >7. Communication</p>
+                    <p>
+                        7.1 We will communicate with the Client during the return process to address any questions or concerns.
+                    </p>
+                </div>
+
+                <div>
+                    <p className=" font-bold " >8. Exclusions</p>
+                    <p>
+                        8.1 The return policy applies solely to YouTube Automation Services. Other products or services provided by GROGRIP MEDIA may have separate return policies, as applicable.
+                    </p>
+                </div>
+
+                <div>
+                    <p className=" font-bold " >9. Changes to the Return Policy</p>
+                    <p>
+                        9.1 GROGRIP MEDIA reserves the right to modify or update this return policy at any time without prior notice. Any changes will be effective upon posting on our website.
+                    </p>
+                </div>
+
             </div>
         </div>
     );
