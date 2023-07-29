0a1ac83fd388c4cea7aca22b58a53b54fef9706c
Rajkiran Kalowar <rajkirankalowar@192.168.1.3>
2023-07-29 17:25:23 +0530

    google-analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,37 @@
 import '../styles/globals.css'
 import { Provider } from 'react-redux';
+import { useEffect } from 'react'
 import Head from 'next/head';
 import store from '../redux/store';
 import { Analytics } from '@vercel/analytics/react';
 import SEO from '../components/SEO';
+import ReactGA from 'react-ga';
 
 function MyApp({ Component, pageProps }) {
   const title = "Grogrip Power up Your YouTube Success - Automation, SEO, Content Mastery!"
-  const description ="Boost YouTube growth with Grogrip: automation, SEO, content creation. Enhance online presence, drive targeted traffic. Expert strategies for success"
+  const description = "Boost YouTube growth with Grogrip: automation, SEO, content creation. Enhance online presence, drive targeted traffic. Expert strategies for success"
   const keywords = "Grogrip, Grogrip Media, growgrip,#grogrip, #grogrip_media, #Grogrip, Youtube Automation, SEO, Script Writing, Content Writing"
-  const ogImage ='./favicon.ico'
+  const ogImage = './favicon.ico'
+
+  useEffect(() => {
+    // Initialize Google Analytics with your tracking ID
+    ReactGA.initialize('G-RQF44EP1R1');
+
+    // Send initial pageview event
+    ReactGA.pageview(window.location.pathname);
+
+    // Track pageview on route change
+    const handleRouteChange = (url) => {
+      ReactGA.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    // Clean up event listener
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, []);
+
   return (
     <>
       <Head>
