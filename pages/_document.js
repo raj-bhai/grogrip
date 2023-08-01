1e2711fdc805b81c2f1061fd63f547bfb6e2031a
Rajkiran Kalowar <rajkirankalowar@Rajkirans-MacBook-Pro.local>
2023-08-01 15:44:49 +0530

    GA

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,17 @@ export default class MyDocument extends Document {
                 <Head>
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Lato&family=Yatra+One&display=swap" rel="stylesheet" />
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-RQF44EP1R1');
+            `,
+                        }}
+                    />
                 </Head>
                 <body>
                     <Main />
