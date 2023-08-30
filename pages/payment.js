f1eb0994beba32c983bf13d4711e65b2a1176cb9
Rajkiran Kalowar <rajkirankalowar@192.168.1.8>
2023-08-30 23:42:26 +0530

    stripe-test-credential

--- pages/payment.js
+++ pages/payment.js
@@ -4,7 +4,9 @@ import { Elements } from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
 import { useRouter } from 'next/router';
 
-const stripePromise = loadStripe('pk_live_51NEKkzSJE0NizD7jCjuxJMXNzzEPfIavLwvSxpcCRE0u122lNA4ljd8xHz0Rpr9QMR8BYPl2Ks4w741Vw84dBipt00eC4fEnFs');
+
+// const stripePromise = loadStripe('pk_live_51NEKkzSJE0NizD7jCjuxJMXNzzEPfIavLwvSxpcCRE0u122lNA4ljd8xHz0Rpr9QMR8BYPl2Ks4w741Vw84dBipt00eC4fEnFs');   //Live
+const stripePromise = loadStripe('pk_test_51NEKkzSJE0NizD7jHYUxCy0VyrzWBIJD9U3DI69WijOprDpZSWpKycyXTxoDGAPN3feqLCpZTSwT5efy27eZ68fk00tfxpU8aX');    //Test
 
 const CheckoutScreen = () => {
     const router = useRouter()
